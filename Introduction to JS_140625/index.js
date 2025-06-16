76fb4f8a11423f11b78d27318e17bed4261ef066
Saket Ranjan <161413274+Saket22Ranjan@users.noreply.github.com>
2025-06-16 23:19:22 +0530

    index.js

---
+++ Introduction to JS_140625/index.js
@@ -0,0 +1,59 @@
+//var s1 = "Rishabh";
+//console.log(s1);
+
+//var s1 = "Ayush";
+
+// console.log(s1);
+
+//let s2 ="Suraj";
+
+//console.log(s2);
+
+//let s2 ="Piyush";
+
+//console.log(s2);
+
+// Lexical Scope
+
+//var a = 4000;
+
+//{
+    //var a = 100;
+    //var b= 200;
+  //  var c = 300;
+
+//}
+//console.log(a);
+
+//console.log(b);
+
+//console.log(c);//
+
+// closure in js
+
+//function z(){
+    //var b=900;
+    //function x(){
+        //var a =7;
+
+        //function y(){
+         //   console.log(a,b);
+       // }
+     //   y();
+   // }
+ //   x();
+//}
+//z();
+// settimeout
+
+//setTimeout(()=>{
+  //  console.log("Hello I am setTimeout Function I'll after 2 secs");
+//},2000);
+
+//SERTINTERVAL
+
+for (var i=0; i<=10;i++){
+    setTimeout(()=>{
+        console.log(i);
+    },2000)
+}
