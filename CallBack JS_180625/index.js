b2c895dd2c2781d8528c2eb37118657d5004078f
Saket Ranjan <161413274+Saket22Ranjan@users.noreply.github.com>
2025-06-19 15:43:06 +0530

    Create index.js

---
+++ CallBack JS_180625/index.js
@@ -0,0 +1,142 @@
+// const { useCallback } = require("react");
+
+// function serveWater(callback){
+//     console.log("Serving water to the group");
+//     setTimeout(Callback,1000);
+// }
+
+// function servesoup(callback){
+//     console.log('serving soup to the group');
+//     setTimeout(callback,1000);
+// }
+
+// function serveStarters(callback){
+//     console.log("serving starter to the group");
+//     setTimeout(callback,1000);
+// }
+
+// function serveDrinks(callback){
+//     console.log("serving drinks to the group");
+//     setTimeout(callback,1000);
+// }
+
+// function servedinner(callback){
+//     console.log("serving dinner to the group");
+//     setTimeout(callback,1000);
+// }
+
+// function paybill(){
+//     console.log("paying the bill & out of the hotel");
+// }
+
+// serveWater(()=>{
+//     servesoup(()=>{
+//         serveStarters(()=>{
+//             serveDrinks(()=>{
+//                 servedinner(()=>{
+//                     paybill();
+//                 })
+//             })
+//         })
+//     })
+// })
+
+// micro vs macro
+// console.log("Program Start");
+
+// setTimeout (()=>{
+// console.log("I am SetTimeout");
+// },5000);
+
+// fetch("https://dummyjson.com/products"). then(function productDet(){
+// console.log("DATA : SOMETHING");
+
+// });
+
+// // console. log("Program End");
+
+// // Higher Order Function
+
+// const data1 = [2,5,3,6]
+
+// const data2 = [5,7,43,7]
+
+// const calculateDouble = function (x){
+//     let output = [];
+//      for (let i=0; i<x.length; i++){
+//         output.push(x[i]*x[i]);
+//     }
+//     return output;
+
+// }
+
+// console.log(calculateDouble(data1));
+// console.log(calculateDouble(data2));
+
+// const calculateHalf = function (data){
+//     let output =[]
+//     for (let i = 0; i<data. length; i++){
+//         output.push(data[i]/2);
+//     }
+// return output;
+// }
+
+// console.log(calculateHalf(data1));
+// console.log(calculateHalf(data2));
+
+// // MAP
+
+// const arr =[5,1,4,6];
+
+
+// function double(x){
+// return x*2
+// }
+
+// function triple(x){
+// return x*3
+
+// }
+
+// const output = arr.map(double);
+
+// console.log(output);
+
+// const output1 = arr.map(triple);
+
+// console.log(output1);
+
+// function double(x){
+// return x*2
+
+// }
+
+// Array. prototype. calculate = function (logic){
+//     let output = []
+//     for (let i =0; i<this. length; i++){
+//         output.push(logic(this[i]));
+//     }
+//     return output;
+// }
+
+// const arr1 = [1,2,3,4]
+// const arr2 = [4,3,2,1]
+// const output1 = arr1.calculate(double);
+// console.log(output1);
+
+// const output2 = arr2.calculate(double);
+// console.log(output2);
+
+
+// filter
+
+
+const arr =[2,,3,4,5,6,7,8]
+
+function isOdd(x){
+    return x%2
+}
+
+const output = arr.filter(isOdd);
+
+console.log(output);
